0c120a5947c974a04d4e010a308b8159c4b9a9bd
franz151 <trevor@saberemail.com>
2014-09-18 19:36:12 -0700

    First upload

---
+++ OpperationsOnVaribles.playground/section-1.swift
@@ -0,0 +1,29 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+typealias WholeNumber = Int
+typealias NumberWithDecimal = Double
+
+var x:WholeNumber = 49
+var y = 10
+
+var additionAnswer = x + y
+var subtrationAnswer = x - y
+var multiplcationAnswer = x * y
+var divisionAnswer = x / y
+
+var discountOnShoesAtStore = 0.3
+var priceOfshoes:NumberWithDecimal = 33
+
+var priceAfterDiscount = priceOfshoes * (1 - discountOnShoesAtStore)
+
+var downPayment = 10.5
+
+priceAfterDiscount = priceAfterDiscount - downPayment
+
+var z = 33
+z = 44
+
+var 😃 = 99
+var 😢 = 😃 + z
